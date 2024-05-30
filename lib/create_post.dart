1e8a8c3fa1bf6987cffc61ef289277d871240b27
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2024-05-30 16:19:16 +0530

    post uploading in the firebase

--- lib/create_post.dart
+++ lib/create_post.dart
@@ -1,8 +1,11 @@
-import 'dart:ffi';
 import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart';
+import 'package:social_media/main.dart';
+import 'package:social_media/services/auth_services.dart';
+import 'package:social_media/services/storage_services.dart';
 
 class CreatePost extends StatefulWidget {
   const CreatePost({super.key});
@@ -18,6 +21,9 @@ class _CreatePostState extends State<CreatePost> {
   final descController = TextEditingController();
   final _formkey = GlobalKey<FormState>();
 
+  final _store = locator<StorageServices>();
+  final user = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
     uploadImageGallary() async {
@@ -30,8 +36,7 @@ class _CreatePostState extends State<CreatePost> {
         if (image != null) {
           _image = File(image.path);
           print(_image);
-        }
-        else{
+        } else {
           print('No image selected.');
         }
       });
@@ -47,166 +52,183 @@ class _CreatePostState extends State<CreatePost> {
         if (image != null) {
           _image = File(image.path);
           print(_image);
-        }
-        else{
+        } else {
           print('No image selected.');
         }
       });
     }
 
-    uploadImageDialog(){
+    createPost(){
+      _store.createPost(
+          title: titleController.text,
+          description: descController.text,
+          file: _image!,
+          user: user!.uid
+      ).then((_) => {
+        titleController.clear(),
+        descController.clear(),
+      }).catchError((e)=>print(e));
+    }
+
+    uploadImageDialog() {
       showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Center(child: Text('Upload Image')),
-            content: SizedBox(
-              height: 50,
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        uploadImageGallary().then((value) => Navigator.pop(context));
-                      },
-                      child: const Text('Gallery'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        uploadImageCamera();
-                      },
-                      child: const Text('Camera'),
-                    )
-                  ],
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Center(child: Text('Upload Image')),
+              content: SizedBox(
+                height: 50,
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          uploadImageGallary()
+                              .then((value) => Navigator.pop(context));
+                        },
+                        child: const Text('Gallery'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          uploadImageCamera();
+                        },
+                        child: const Text('Camera'),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
-        }
-      );
+            );
+          });
     }
 
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Create Post',
-              style: TextStyle(
-                  fontSize: 40,
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w600),
-            ),
-            const SizedBox(height: 30),
-            Form(
-              key: _formkey,
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter title';
-                        }
-                        return null;
-                      },
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      controller: titleController,
-                      decoration: const InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          borderSide: BorderSide(
-                              color: Color.fromARGB(255, 45, 210, 90)),
-                        ),
-                        label: Text('Title'),
-                        labelStyle: TextStyle(
-                            color: Color(0xffC4C3C3), fontFamily: 'Poppins'),
-                        floatingLabelStyle: TextStyle(color: Color(0xff4DD969)),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          borderSide: BorderSide(color: Color(0xffeeeeee)),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter description';
-                        }
-                        return null;
-                      },
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      controller: descController,
-                      decoration: const InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          borderSide: BorderSide(
-                              color: Color.fromARGB(255, 45, 210, 90)),
-                        ),
-                        label: Text('Description'),
-                        labelStyle: TextStyle(
-                            color: Color(0xffC4C3C3), fontFamily: 'Poppins'),
-                        floatingLabelStyle: TextStyle(color: Color(0xff4DD969)),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          borderSide: BorderSide(color: Color(0xffeeeeee)),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Create Post',
+                style: TextStyle(
+                    fontSize: 40,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(height: 30),
+              Form(
+                key: _formkey,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter title';
+                          }
+                          return null;
+                        },
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: titleController,
+                        decoration: const InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                                color: Color.fromARGB(255, 45, 210, 90)),
+                          ),
+                          label: Text('Title'),
+                          labelStyle: TextStyle(
+                              color: Color(0xffC4C3C3), fontFamily: 'Poppins'),
+                          floatingLabelStyle: TextStyle(color: Color(0xff4DD969)),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(color: Color(0xffeeeeee)),
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: const Color.fromARGB(255, 45, 210, 90),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter description';
+                          }
+                          return null;
+                        },
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: descController,
+                        decoration: const InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                                color: Color.fromARGB(255, 45, 210, 90)),
+                          ),
+                          label: Text('Description'),
+                          labelStyle: TextStyle(
+                              color: Color(0xffC4C3C3), fontFamily: 'Poppins'),
+                          floatingLabelStyle: TextStyle(color: Color(0xff4DD969)),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(color: Color(0xffeeeeee)),
+                          ),
                         ),
                       ),
-                      child: _image == null
-                          ? const Center(
-                              child: Text('No Image Selected'),
-                            )
-                          : Image.file(_image!),
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        uploadImageDialog();
-                      },
-                      child: const Text('Upload Image'),
-                    ),
-                    const SizedBox(height: 30,),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 45, 210, 90),
-                        // minimumSize: const (380, 60),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: const Color.fromARGB(255, 45, 210, 90),
+                          ),
                         ),
+                        child: _image == null
+                            ? const Center(
+                                child: Text('No Image Selected'),
+                              )
+                            : Image.file(_image!),
                       ),
-                      onPressed: () {
-                        if (_formkey.currentState!.validate()) {
-                        }
-                      },
-                      child: const Text(
-                        'Create Post',
-                        style: TextStyle(
-                            fontSize: 14,
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          uploadImageDialog();
+                        },
+                        child: const Text('Upload Image'),
                       ),
-                    )
-                  ],
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 45, 210, 90),
+                          minimumSize: const Size(380, 60),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () {
+                          if (_formkey.currentState!.validate()) {
+                            createPost();
+                          }
+                          _image = null;
+                          setState((){});
+                        },
+                        child: const Text(
+                          'Create Post',
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/services/storage_services.dart
+++ lib/services/storage_services.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'dart:io';
 
@@ -19,6 +20,47 @@ class StorageServices{
     return downloadUrl;
     } catch (e) {
       print(e.toString());
+    }
+  }
+
+  Future<String?> uploadPostPicture(String fileName, File file) async {
+    print(fileName);
+    try {
+    Reference ref = _storage.ref().child("postPicture/${fileName}");
+    UploadTask uploadImage = ref.putData(await file.readAsBytes());
+    uploadImage.snapshotEvents.listen((event) {
+      print(event.state);
+    });
+    TaskSnapshot snapshot = await uploadImage.whenComplete(() => null);
+    print("Test 2");
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+    print(downloadUrl);
+    return downloadUrl;
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  final userCollectionRef = FirebaseFirestore.instance.collection("posts");
+
+  Future<void> createPost({
+    required String title,
+    required String description,
+    required String user,
+    required File file,
+  }) async {
+    try{
+      String? imageUrl = await uploadPostPicture('$user.png', file);
+      await userCollectionRef.add({
+        'title': title,
+        'description': description,
+        'createdBy': user,
+        'profileImage': imageUrl,
+        'createdAt': Timestamp.now()
+      });
+    }
+    catch(e){
+      print(e);
     }
   }
 }
